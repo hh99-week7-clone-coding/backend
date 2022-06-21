942c9f6609371af3166b4661cff43fa2108c745a
gaius365 <gaius365@gmail.com>
2022-06-21 18:30:11 +0900

    edit: 신규 RDS 생성에 따른 설정 변경

--- src/main/java/com/sparta/hh99_clone/controller/VehicleController.java
+++ src/main/java/com/sparta/hh99_clone/controller/VehicleController.java
@@ -5,7 +5,6 @@ import com.sparta.hh99_clone.service.VehicleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
