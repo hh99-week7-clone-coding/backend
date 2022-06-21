92070d68b556ced9e18382c14c42c935c68248c5
Mo-Greene <13blueboy13@daum.net>
2022-06-21 15:58:34 +0900

    add 로그인 exception

--- src/main/java/com/sparta/hh99_clone/security/UserDetailsServiceImpl.java
+++ src/main/java/com/sparta/hh99_clone/security/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.sparta.hh99_clone.security;
 
 import com.sparta.hh99_clone.domain.User;
+import com.sparta.hh99_clone.exception.CustomException;
 import com.sparta.hh99_clone.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,7 +21,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Invalid Email Address"));
+                .orElseThrow(() -> new UsernameNotFoundException("Can't find " + username));
 
         return new UserDetailsImpl(user);
     }
