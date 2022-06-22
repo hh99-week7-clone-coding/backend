5033e9ebab91ad41ef1e2d037b8e90a79c730053
gaius365 <gaius365@gmail.com>
2022-06-22 17:51:32 +0900

    edit: Item 카테고리 조회 URL 변경
    @PathVariable 적용

--- src/main/java/com/sparta/hh99_clone/controller/ItemController.java
+++ src/main/java/com/sparta/hh99_clone/controller/ItemController.java
@@ -14,8 +14,8 @@ public class ItemController {
     private final ItemService itemService;
 
     // Item 카테고리 조회
-    @GetMapping("/api/shop/category")
-    public ResponseEntity<List<ItemResponseDto>> getItems(@RequestParam(required = false) String categoryId) {
+    @GetMapping("/api/shop/category/{categoryId}")
+    public ResponseEntity<List<ItemResponseDto>> getItems(@PathVariable String categoryId) {
         return ResponseEntity.ok().body(itemService.getItems(categoryId));
     }
 
